edba79c8a81006438b06efd22a6a0036f2087181
Rainer Stropek <rainer@software-architects.at>
2019-10-02 16:21:46 +0200

    Add logging sample

---
+++ aspnet-core/0050-logging/Controllers/MathController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo.Controllers
+{
+    [ApiController]
+    [Route("api/math")]
+    public partial class MathController : ControllerBase
+    {
+        private readonly ILogger<MathController> _logger;
+
+        public MathController(ILogger<MathController> logger) =>
+            _logger = logger;
+    }
+}
---
+++ aspnet-core/0050-logging/Controllers/MathControllerDiv.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo.Controllers { 
+    public partial class MathController {
+        [HttpGet]
+        [Route("div")]
+        public IActionResult Div([FromQuery] int x, [FromQuery] int y)
+        {
+            if (y == 0) {
+                _logger.LogError("Tried to divide by zero");
+                return BadRequest("Division by zero not possible");
+            }
+            if (x % y != 0) {
+                _logger.LogWarning("Non-zero remainder, possible data loss");
+            }
+            return Ok(x / y);
+        }
+    }
+}
